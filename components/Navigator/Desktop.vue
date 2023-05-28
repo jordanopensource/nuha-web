6254a134b0c77e3e70e9c3147b8425a6f28bef41
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-05-28 14:15:29 +0300

    feat: added desktop navigator

--- components/Navigator/Desktop.vue
+++ components/Navigator/Desktop.vue
@@ -1,3 +1,31 @@
 <template>
-  <div>desktop</div>
+  <div class="px-[40px] py-[10px] pt-[40px] text-nuha-fushia">
+    <div class="flex justify-between">
+      <img width="75" height="75" src="/logo.svg" />
+
+      <div class="flex justify-between items-center">
+        <div class="text-[20px] font-[300] px-[30px]">
+          <NuxtLink class="px-[20px]" to="statistics">{{
+            $t('link.statistics')
+          }}</NuxtLink>
+          &VerticalBar;
+          <NuxtLink class="px-[20px]" to="learn-more">{{
+            $t('link.learnMore')
+          }}</NuxtLink>
+        </div>
+        <NuxtLink
+          class="inline-block rounded-[10px] border-[1px] p-[8px] text-[18px] px-[16px] border-nuha-fushia flex items-center"
+          :to="switchLocalePath($t('otherLocaleCode'))"
+          >{{ $t('otherLocale') }}</NuxtLink
+        >
+      </div>
+    </div>
+    <div
+      class="block my-[10px] border-t-[1px] border-t-nuha-fushia-light h-[0.5px] w-full"
+    />
+  </div>
 </template>
+
+<script lang="ts" setup>
+  const switchLocalePath = useSwitchLocalePath()
+</script>
