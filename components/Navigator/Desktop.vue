464d8f320c8e9c9ca6b1213952815a57d9196637
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-05-30 13:17:20 +0300

    fix: postcss in desktop

--- components/Navigator/Desktop.vue
+++ components/Navigator/Desktop.vue
@@ -1,31 +1,60 @@
 <template>
-  <div class="px-[40px] py-[10px] pt-[40px] text-nuha-fushia">
-    <div class="flex justify-between">
+  <div class="container">
+    <div class="container_sub">
       <img width="75" height="75" src="/logo.svg" />
 
-      <div class="flex justify-between items-center">
-        <div class="text-[20px] font-[300] px-[30px]">
-          <NuxtLink class="px-[20px]" to="statistics">{{
+      <div class="link">
+        <div class="link_nav">
+          <NuxtLink class="link_nav_item" to="statistics">{{
             $t('link.statistics')
           }}</NuxtLink>
           &VerticalBar;
-          <NuxtLink class="px-[20px]" to="learn-more">{{
+          <NuxtLink class="link-nav-item" to="learn-more">{{
             $t('link.learnMore')
           }}</NuxtLink>
         </div>
+
         <NuxtLink
-          class="inline-block rounded-[10px] border-[1px] p-[8px] text-[18px] px-[16px] border-nuha-fushia flex items-center"
+          class="link_locale"
           :to="switchLocalePath($t('otherLocaleCode'))"
           >{{ $t('otherLocale') }}</NuxtLink
         >
       </div>
     </div>
-    <div
-      class="block my-[10px] border-t-[1px] border-t-nuha-fushia-light h-[0.5px] w-full"
-    />
+    <div class="header-separator" />
   </div>
 </template>
 
 <script lang="ts" setup>
   const switchLocalePath = useSwitchLocalePath()
 </script>
+
+<style lang="postcss" scoped>
+  .container {
+    @apply px-10 py-2.5 pt-10 text-nuha-fushia;
+
+    &_sub {
+      @apply flex justify-between;
+    }
+  }
+
+  .link {
+    @apply flex justify-between items-center;
+
+    &_locale {
+      @apply inline-block rounded-xl border p-2 text-xl px-3.5 border-nuha-fushia flex items-center;
+    }
+
+    &_nav {
+      @apply text-xl font-light px-7;
+
+      &_item {
+        @apply px-5;
+      }
+    }
+  }
+
+  .header-separator {
+    @apply block my-2.5 border-t border-t-nuha-fushia-light h-px w-full;
+  }
+</style>
