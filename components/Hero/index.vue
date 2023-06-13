7e32302555aee4e03ca9bc756633d0ec81fd6f8d
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-06-13 16:30:50 +0300

    chore: added hero section's text

---
+++ components/Hero/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="main">
+    <HeroIntroText />
+    <div>
+      <HeroWaitlist />
+      <HeroLearnMore />
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+  .main {
+    @apply h-5/6 grid grid-cols-1 place-items-center md:flex md:items-center md:justify-between;
+  }
+</style>
