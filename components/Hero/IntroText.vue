c08e30c81384e989caf9b97f2bb8a9c95594daf9
thamudi <tamim.hamoudi@gmail.com>
2023-06-14 21:40:52 +0300

    fix: intro text styling

--- components/Hero/IntroText.vue
+++ components/Hero/IntroText.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="text">
-    <h1 class="main">{{ $t('intro.text') }}</h1>
-    <span>&nbsp;</span>
-    <h2 class="sub">{{ $t('intro.sub') }}</h2>
+    <h1>
+      <span class="main">
+        {{ $t('intro.text') }}
+      </span>
+      <span class="sub">
+        {{ $t('intro.sub') }}
+      </span>
+    </h1>
   </div>
 </template>
 
 <style lang="postcss" scoped>
   .text {
-    @apply text-nuha-fushia-300 px-10 text-4xl md:text-6xl italic leading-snug;
+    @apply text-nuha-fushia-300 px-10 text-4xl lg:text-6xl italic leading-snug;
     .main {
-      @apply inline font-thin;
+      @apply inline font-light;
     }
 
     .sub {
-      @apply inline font-bold;
+      @apply inline font-semibold font-IBMPlexSansArabic;
     }
   }
 </style>
