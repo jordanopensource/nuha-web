5fc654dcde418a33d39ca0c48ae27d7681b78d56
thamudi <tamim.hamoudi@gmail.com>
2023-06-17 09:27:27 +0300

    feat: add nuha logo in both pink and white

--- components/Ui/AppHeader/Mobile.vue
+++ components/Ui/AppHeader/Mobile.vue
@@ -1,25 +1,53 @@
 <template>
-  <div class="container">
+  <div
+    class="container"
+    :class="
+      props.logoColor !== 'white' ? 'text-nuha-fushiaz' : 'text-nuha-fushia-100'
+    "
+  >
     <div class="container-sub">
       <NuxtLink
         v-for="localeItem in availableLocales"
         :key="localeItem.code"
         :to="switchLocalePath(localeItem.code)"
         class="locale-link"
-        :class="localeItem.code"
+        :class="
+          (localeItem.code,
+          props.logoColor !== 'white'
+            ? 'border-nuha-fushia text-nuha-fushia'
+            : 'border-nuha-fushia-100')
+        "
         >{{ localeItem.name }}
       </NuxtLink>
 
       <NuxtLink to="/">
-        <img width="50" height="50" src="/logo.svg" />
+        <img
+          width="50"
+          height="50"
+          :src="props.logoColor !== 'white' ? '/logo.svg' : 'logo-white.svg'"
+        />
       </NuxtLink>
     </div>
-    <div class="header-separator"></div>
+    <div
+      class="header-separator"
+      :class="
+        props.logoColor !== 'white'
+          ? 'border-nuha-fushia-300 '
+          : 'border-nuha-fushia-100'
+      "
+    ></div>
   </div>
 </template>
 
 <script lang="ts" setup>
   import { LocaleObject } from '@nuxtjs/i18n/dist/runtime/composables'
+
+  const props = defineProps({
+    logoColor: {
+      type: String,
+      default: 'default',
+    },
+  })
 
   const { locale, locales } = useI18n()
   const switchLocalePath = useSwitchLocalePath()
@@ -32,7 +60,7 @@
 
 <style lang="postcss" scoped>
   .container {
-    @apply py-2.5 pt-10 text-nuha-fushia antialiased;
+    @apply py-2.5 pt-10  antialiased;
 
     &-sub {
       @apply flex justify-between;
@@ -40,10 +68,10 @@
   }
 
   .locale-link {
-    @apply rounded-xl border p-0.5 px-2 border-nuha-fushia flex items-center rtl:font-IBMPlexMono ltr:font-IBMPlexSansArabic;
+    @apply rounded-xl border p-0.5 px-2  flex items-center rtl:font-IBMPlexMono ltr:font-IBMPlexSansArabic;
   }
 
   .header-separator {
-    @apply block my-2.5 border-t border-t-nuha-fushia-300 h-px w-full;
+    @apply block my-2.5 border-t h-px w-full;
   }
 </style>
