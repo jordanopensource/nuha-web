e895075b84d7b3dbf47c607dc906f9b96025db85
thamudi <tamim.hamoudi@gmail.com>
2023-06-17 09:27:05 +0300

    refactor: add new prop to check which logo color to choose from

--- components/Ui/AppHeader/index.vue
+++ components/Ui/AppHeader/index.vue
@@ -1,6 +1,16 @@
 <template>
   <header>
-    <div class="lg:hidden"><UiAppHeaderMobile /></div>
+    <div class="lg:hidden">
+      <UiAppHeaderMobile :logo-color="props.logoColor" />
+    </div>
     <div class="hidden lg:block"><UiAppHeaderDesktop /></div>
   </header>
 </template>
+<script setup>
+  const props = defineProps({
+    logoColor: {
+      type: String,
+      default: 'default',
+    },
+  })
+</script>
