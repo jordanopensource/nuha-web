1dadd6b73098b04bc1bf78577d60e87e2d1fd067
thamudi <tamim.hamoudi@gmail.com>
2023-06-18 13:07:47 +0300

    fix: general style fixes
    move learn-more id to parent container, move container class to parent container

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
   <div
-    class="snap-y snap-mandatory h-screen w-full overflow-scroll lg:snap-none lg:overflow-x-hidden"
+    class="snap-y snap-mandatory h-screen overflow-scroll lg:snap-none lg:overflow-x-hidden"
   >
-    <div class="h-screen">
-      <UiAppHeader class="container snap-start shrink-0" />
+    <div class="h-screen container">
+      <UiAppHeader class="snap-start shrink-0" />
       <Hero class="container" />
     </div>
     <StatisticsIndicator />
     <Statistics id="statistics" class="snap-start shrink-0" />
 
-    <div class="container lg:my-60 mb-60">
-      <NuhaIdentity id="learn-more" class="snap-start shrink-0" />
-      <NuhaWork class="container snap-start shrink-0" />
+    <div id="learn-more" class="container lg:my-60 mb-60">
+      <NuhaIdentity class="snap-start shrink-0" />
+      <NuhaWork class="snap-start shrink-0" />
     </div>
   </div>
 </template>
