4bdf01e0f0163c3235577fbf5b376e14d22c32f0
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-07-03 13:41:27 +0300

    fix: fixed how the user menu looks on desktop

--- components/UserMenu/Items/index.vue
+++ components/UserMenu/Items/index.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="dashboard-menu">
-    <NuxtLink class="dashboard-menu-item" to="/dashboard">{{
-      $t('dashboard.menu.dashboard')
-    }}</NuxtLink>
+  <div class="lg:absolute">
+    <div class="dashboard-menu">
+      <NuxtLink class="dashboard-menu-item" to="/dashboard">{{
+        $t('dashboard.menu.dashboard')
+      }}</NuxtLink>
 
-    <UserMenuItemsSeparator />
+      <UserMenuItemsSeparator />
 
-    <NuxtLink class="dashboard-menu-item" to="/dashboard/settings">{{
-      $t('dashboard.menu.settings')
-    }}</NuxtLink>
+      <NuxtLink class="dashboard-menu-item" to="/dashboard/settings">{{
+        $t('dashboard.menu.settings')
+      }}</NuxtLink>
 
-    <UserMenuItemsSeparator />
+      <UserMenuItemsSeparator />
 
-    <button class="dashboard-menu-item" @click="signOut">
-      {{ $t('dashboard.menu.signOut') }}
-    </button>
+      <button class="dashboard-menu-item" @click="signOut">
+        {{ $t('dashboard.menu.signOut') }}
+      </button>
+    </div>
   </div>
 </template>
 
@@ -26,15 +28,17 @@
 
 <style lang="postcss" scoped>
   .dashboard-menu {
-    @apply absolute bg-nuha-fushia-100 top-28;
-    @apply ltr:right-0 lg:ltr:right-16;
-    @apply rtl:left-0 lg:rtl:left-16;
+    @apply absolute lg:relative bg-nuha-fushia-100 top-28 lg:top-5;
+    @apply ltr:right-0 lg:ltr:right-10;
+    @apply rtl:left-0 lg:rtl:left-10;
     @apply w-full lg:w-auto;
     @apply grid grid-cols-1 place-items-center;
+    @apply lg:border lg:border-nuha-fushia lg:rounded-xl;
   }
 
   .dashboard-menu-item {
     @apply block p-4 text-nuha-fushia;
     @apply lg:hover:bg-nuha-fushia-200;
+    @apply lg:first:rounded-t-xl lg:last:rounded-b-xl;
   }
 </style>
