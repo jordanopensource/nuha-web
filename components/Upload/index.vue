f39af8336b00a107decb80b682afa88afbc97e75
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-07-04 15:02:54 +0300

    chore: added data upload loading spinner

--- components/Upload/index.vue
+++ components/Upload/index.vue
@@ -23,21 +23,31 @@
     />
 
     <button
-      class="w-full border-2 rounded-xl bg-nuha-fushia-300 text-white text-2xl border-nuha-grey p-3"
+      class="w-full border-2 rounded-xl bg-nuha-fushia-300 text-white text-2xl border-nuha-grey p-3 flex items-center justify-center"
       @click="handleSubmit"
     >
-      {{ t('dashboard.submit') }}
+      <div v-if="!loading">
+        {{ t('dashboard.submit') }}
+      </div>
+      <div v-else class="loader my-8"></div>
     </button>
   </div>
 </template>
 
 <script setup lang="ts">
   import RequestBody from './RequestBody'
+  import { set } from '@vueuse/core'
 
   const { t } = useI18n()
   const data = ref({} as RequestBody)
+  const loading = ref(false)
 
   async function handleSubmit() {
+    set(loading, true)
+
+    // TODO: upload logic goes here
     console.log(data)
+
+    setTimeout(() => set(loading, false), 2500)
   }
 </script>
