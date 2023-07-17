a38d13773ceb1131bfe6e8569369fa9161063a26
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-07-17 11:37:07 +0300

    patch: fixed incorrect redirection to /api/auth
    the auth middleware redirects to /api/auth, when navigating to /dashboard manually, instead of redirecting to /login

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -4,6 +4,9 @@ import GithubProvider from 'next-auth/providers/github'
 const runtimeConfig = useRuntimeConfig()
 
 export default NuxtAuthHandler({
+  pages: {
+    signIn: '/login',
+  },
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GithubProvider.default({
