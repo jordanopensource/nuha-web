7e8f3501399be7fdda502bd65b0f374bd621467b
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-07-18 13:50:42 +0300

    fix: disabled the hide menu composable's behavior on mobile

---
+++ components/UserMenu/Button.vue
@@ -0,0 +1,60 @@
+<template>
+  <NuxtLink :to="localePath('/login')" v-if="!isLoggedIn()" class="login-btn">
+    {{ t('header.login') }}
+  </NuxtLink>
+  <div v-else>
+    <button
+      @click="
+        () => {
+          emit('update:showItems', !showItems)
+          showItems = !showItems
+        }
+      "
+      class="lg:ltr:pl-8 lg:rtl:pr-8 text-nuha-fushia"
+    >
+      <nuxt-img
+        :src="
+          props.logoColor === 'white'
+            ? '/burger-menu-white.svg'
+            : '/burger-menu.svg'
+        "
+        class="w-10 h-10"
+      />
+    </button>
+  </div>
+</template>
+
+<script setup>
+  import { set } from '@vueuse/core'
+  import { ref } from 'vue'
+
+  const emit = defineEmits(['update:showItems'])
+
+  const { t } = useI18n()
+  const hideMenu = useHideMenu()
+  const showItems = ref(false)
+  watch(hideMenu, (value) => {
+    if (value) {
+      set(showItems, false)
+      emit('update:showItems', false)
+    }
+  })
+
+  const props = defineProps({
+    logoColor: {
+      type: String,
+      default: 'default',
+    },
+  })
+  const localePath = useLocalePath()
+
+  function isLoggedIn() {
+    return useAuthCheck()
+  }
+</script>
+
+<style lang="postcss" scoped>
+  .login-btn {
+    @apply rounded-xl p-2 lg:rtl:mr-2 lg:ltr:ml-2 text-lg border border-nuha-fushia text-nuha-fushia flex items-center ltr:font-IBMPlexMono rtl:font-IBMPlexSansArabic;
+  }
+</style>
--- composables/useHideMenu.ts
+++ composables/useHideMenu.ts
@@ -4,7 +4,7 @@ export default function () {
   const { $useScroll } = useNuxtApp()
   if (typeof $useScroll === 'function') {
     const { y } = $useScroll()
-    return computed(() => get(y) !== 0)
+    return computed(() => get(y) !== 0 && window.outerWidth > 1024)
   }
   return computed(() => false)
 }
