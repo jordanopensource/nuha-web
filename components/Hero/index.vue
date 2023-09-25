ca80020e71c6198869a1ab6705842827dc7757e6
Baraa Al-Masri <pub@mbaraa.com>
2023-09-25 22:09:55 +0300

    chore: refactored home's hero section

--- components/Hero/index.vue
+++ components/Hero/index.vue
@@ -1,27 +1,48 @@
-<script setup lang="ts"></script>
-
 <template>
   <div class="main">
-    <HeroIntroText v-if="!hideSection" />
-    <div class="flex-grow">
-      <HeroWaitlist @hide-intro="(value) => (hideSection = value)" />
-      <div class="lg:hidden">
-        <a href="#learn-more" class="link" v-if="!hideSection">
-          {{ $t('link.learnMore') }}
-        </a>
+    <div class="w-full">
+      <div class="w-3/4">
+        <h1 class="text text-main text-6xl lg:text-8xl">
+          {{ $t('homeHero.title') }}
+        </h1>
+        <h2 class="text text-sub text-3xl lg:text-4xl">
+          {{ $t('homeHero.intro') }}
+        </h2>
+        <button class="btn">
+          Try it out
+          <IconArrowRight class="rtl:hidden ltr:inline-block" />
+          <IconArrowLeft class="ltr:hidden rtl:inline-block" />
+        </button>
       </div>
+    </div>
+    <div class="w-full">
+      <NuxtImg class="nuha-logo" src="/hero-logo.svg" />
     </div>
   </div>
 </template>
-<script setup lang="ts">
-  import { set } from '@vueuse/core'
-  const hideSection = ref(false)
-</script>
+
+<script setup lang="ts"></script>
+
 <style lang="postcss" scoped>
   .main {
     @apply h-5/6 grid grid-cols-1 place-items-center lg:flex lg:items-center lg:justify-between lg:gap-x-14;
   }
   .link {
     @apply grid grid-cols-1 place-items-center pt-7 italic text-xl font-medium underline text-nuha-fushia;
+  }
+  .text {
+    @apply text-nuha-grey leading-snug font-IBMPlexSansArabic;
+  }
+  .btn {
+    @apply border my-3 p-3 flex items-center justify-center text-2xl;
+    @apply border-nuha-fushia-300;
+    @apply bg-nuha-fushia-300 text-white;
+    @apply hover:bg-nuha-fushia-100 hover:text-nuha-fushia-300;
+  }
+  .nuha-logo {
+    @apply my-10 lg:my-0;
+    @apply w-full;
+    @apply md:w-[707px] md:h-[707px];
+    @apply lg:w-[1024px] lg:h-[1024px];
   }
 </style>
