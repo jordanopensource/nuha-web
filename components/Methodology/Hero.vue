de6e5e0f2074ad2581a654f03f002ca3910b9178
Mohamed Mansour <mohmans@pm.me>
2023-09-25 17:49:20 +0300

    feat(methodology): add base page

---
+++ components/Methodology/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container flex flex-col gap-y-24 items-center justify-center">
+    <div class="flex flex-row flex-wrap justify-between gap-y-8">
+      <!-- TODO: i18n -->
+      <h2>Methodology</h2>
+      <p class="sm:max-w-[50%]">
+        Our text models are advanced language processing tools that can generate, classify, and summarize text with high levels of coherence and accuracy.
+      </p>
+    </div>
+    <img src="/images/meth-hero.svg" width="838.5" height="323.25" class="w-full"/>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="postcss" scoped></style>
---
+++ pages/methodology/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="container">
+      <!-- TODO: Add AppHeader to default layout project-wide -->
+      <UiAppHeader
+        class="mb-10"
+      />
+    </div>
+    <MethodologyHero />
+  </div>
+</template>
+
+<script setup lang="ts">
+  definePageMeta({ auth: false })
+</script>
+
+<style lang="postcss" scoped></style>
