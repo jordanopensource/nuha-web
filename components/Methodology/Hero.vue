44de3f1cc0a440f23aa5ffaae9bcf0e4d7edb2d3
Mohamed Mansour <mohmans@pm.me>
2023-09-26 17:08:20 +0300

    refactor(methodology): add i18n

--- components/Methodology/Hero.vue
+++ components/Methodology/Hero.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="container flex flex-col gap-y-24 items-center justify-center">
     <div class="flex flex-row flex-wrap justify-between gap-y-8">
-      <!-- TODO: i18n -->
-      <h2>Methodology</h2>
+      <h2>{{ $t('methodology.title') }}</h2>
       <p class="sm:max-w-[50%]">
-        Our text models are advanced language processing tools that can generate, classify, and summarize text with high levels of coherence and accuracy.
+        {{ $t('methodology.description') }}
       </p>
     </div>
     <img src="/images/meth-hero.svg" width="838.5" height="323.25" class="w-full"/>
