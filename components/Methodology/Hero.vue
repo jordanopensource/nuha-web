72772c14d6ac0b2f298586cda0a85c5ccdb4e114
Tamim Hamoudi <tamim.hamoudi@gmail.com>
2023-09-28 14:40:30 +0300

    refactor: headings standards in hero banner

--- components/Methodology/Hero.vue
+++ components/Methodology/Hero.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="container flex flex-col gap-y-24 items-center justify-center">
     <div class="flex flex-row flex-wrap justify-between gap-y-8">
-      <h2>{{ $t('methodology.title') }}</h2>
+      <h1>{{ $t('methodology.title') }}</h1>
       <p class="sm:max-w-[50%]">
         {{ $t('methodology.description') }}
       </p>
     </div>
-    <img src="/images/meth-hero.svg" width="838.5" height="323.25" class="w-full"/>
+    <img
+      src="/images/meth-hero.svg"
+      width="838.5"
+      height="323.25"
+      class="w-full"
+    />
   </div>
 </template>
 
