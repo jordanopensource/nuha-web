1453175fba389557bfa3c67997f3efb4a21d81ce
Baraa Al-Masri <pub@mbaraa.com>
2023-09-28 15:19:34 +0300

    fix: prevented link scan that makes the magic link expire when doing a HEAD request

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -4,6 +4,14 @@ export default defineEventHandler(async (event) => {
   const session = await getServerSession(event)
   const url = getRequestURL(event)
 
+  if (
+    event.method === 'HEAD' &&
+    url.pathname.startsWith('/api/auth/callback/email')
+  ) {
+    setResponseStatus(event, 200)
+    return {}
+  }
+
   if (!session && url.pathname.startsWith('/api/upload')) {
     setResponseStatus(event, 401)
     return { authenticated: false }
