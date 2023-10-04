4558afc784bae1e150f54aab8097edc7f3592ba2
Baraa Al-Masri <pub@mbaraa.com>
2023-10-04 13:01:17 +0300

    fix: fixed the outlook safe link issue

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -4,6 +4,14 @@ export default defineEventHandler(async (event) => {
   const session = await getServerSession(event)
   const url = getRequestURL(event)
 
+  if (
+    event.headers.get('user-agent')?.toLowerCase().includes('oneoutlook') ||
+    event.headers.get('x-native-host')?.toLowerCase().includes('oneoutlook')
+  ) {
+    setResponseStatus(event, 200)
+    return {}
+  }
+
   if (!session && url.pathname.startsWith('/api/upload')) {
     setResponseStatus(event, 401)
     return { authenticated: false }
