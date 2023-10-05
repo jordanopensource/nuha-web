0d95ac6273cfd67475c9db79954df9e5f38b84ce
Tamim Hamoudi <tamim.hamoudi@gmail.com>
2023-10-05 15:59:04 +0300

    refactor: mobile dropdown menu

--- components/Ui/AppHeader/index.vue
+++ components/Ui/AppHeader/index.vue
@@ -1,12 +1,12 @@
 <template>
   <header class="container">
     <div class="nav-container">
-      <NuxtLink :to="localePath('/')">
+      <NuxtLink :to="localePath('/')" @click="showMobileMenu = !showMobileMenu">
         <img width="70" height="70" src="/logo.svg" />
       </NuxtLink>
 
       <button
-        class="mobile-menu-btn"
+        class="lg:hidden"
         aria-label="navigation menu"
         @click="showMobileMenu = !showMobileMenu"
       >
@@ -16,93 +16,48 @@
           height="40"
         />
       </button>
-      <div
-        class="link link-nav"
-        :class="showMobileMenu ? 'mobile-menu' : 'max-sm:!hidden'"
-      >
-        <NuxtLink
-          v-for="localeItem in availableLocales"
-          :key="localeItem.code"
-          :to="switchLocalePath(localeItem.code)"
-          class="link-locale link-hover"
-          :class="localeItem.code"
-          >{{ localeItem.name }}
-        </NuxtLink>
-
-        <NuxtLink
-          v-for="link in links"
-          class="link-nav-item link-hover"
-          :to="link.path"
-        >
-          {{ link.title }}
-        </NuxtLink>
-
-        <!-- User items -->
-        <UserMenuButton
-          @update:showItems="(value: boolean) => {showUserMenu = value}"
-        />
+      <!-- Desktop list -->
+      <div class="list desktop">
+        <UiAppHeaderMenuItems />
       </div>
+    </div>
+    <!-- Mobile list -->
+    <div class="list mobile" :class="showMobileMenu ? 'show' : 'hide'">
+      <UiAppHeaderMenuItems />
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
-  import { LocaleObject } from '@nuxtjs/i18n/dist/runtime/composables'
-
   // language switcher
-  const { locale, locales, t } = useI18n()
-  const switchLocalePath = useSwitchLocalePath()
   const localePath = useLocalePath()
-  const availableLocales = computed(() => {
-    return (locales.value as LocaleObject[]).filter(
-      (l: LocaleObject) => l.code !== locale.value
-    )
-  })
 
-  type Link = {
-    title: string
-    path: string
-  }
-
-  const links = ref<Link[]>([
-    {
-      title: t('methodology.title'),
-      path: localePath('/methodology'),
-    },
-  ])
-
-  const showUserMenu = ref(false)
   const showMobileMenu = ref(false)
 </script>
 
 <style lang="postcss" scoped>
-  .nav-container {
+  .nav-container,
+  .mobile {
     @apply flex justify-between text-nuha-fushia;
-    @apply pb-5 pt-10  max-lg:mb-10;
+    @apply pb-5 pt-10;
     @apply border-nuha-fushia-300 border-b-2;
   }
 
-  .link {
-    @apply text-xl flex justify-between items-center gap-8;
+  .list {
+    @apply gap-8 font-light text-xl;
+    @apply justify-between items-center;
+  }
+  .desktop {
+    @apply hidden lg:flex;
+  }
 
-    &-nav {
-      @apply font-light;
-    }
-    &-hover:hover {
-      @apply underline;
-    }
+  .mobile {
+    @apply flex-col gap-y-5 lg:hidden;
   }
-  .mobile-menu-btn {
-    @apply sm:hidden;
+  .mobile.show {
+    @apply flex;
   }
-  .mobile-menu {
-    @apply absolute top-20 ltr:right-4 rtl:left-4;
-    @apply flex flex-col gap-y-2 p-4;
-    @apply bg-nuha-fushia-100 border border-nuha-fushia-300 shadow-2xl;
-  }
-  @media (min-width: 640px) {
-    .link {
-      @apply flex-row static p-0 border-0 shadow-none;
-    }
+  .mobile.hide {
+    @apply hidden;
   }
 </style>
