f42a4a551d6dbe35b25ae459f2ea59e2a10bcf65
Baraa Al-Masri <pub@mbaraa.com>
2023-10-06 18:54:50 +0300

    chore: added dashboard's hero

---
+++ components/Dashboard/Hero.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-5 my-5 lg:my-20">
+    <h1>{{ t('dashboard.title') }}</h1>
+    <p class="text-xl">{{ t('dashboard.description') }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const { t } = useI18n()
+</script>
+
+<style scoped lang="postcss"></style>
