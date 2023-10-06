8c09afc32bda853880309f23b5246b469cd23f4a
Baraa Al-Masri <pub@mbaraa.com>
2023-10-07 02:53:35 +0300

    fix: fixed updating line's position for every change that might occurr

--- components/Dashboard/Upload/index.vue
+++ components/Dashboard/Upload/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="w-full my-5 lg:my-20" id="upload-container">
-    <div class="flex items-top">
+    <div class="flex items-top min-h-[150px]">
       <div class="w-1/4 inline-block">
         <DashboardUploadStep
           circle-id="step-one"
@@ -8,17 +8,8 @@
           :description="t('dashboard.steps.step1.name')"
         />
       </div>
-      <div class="w-3/4 inline-block bg-green-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-        laborum sunt ut et earum debitis, harum sequi molestiae odit corporis
-        doloribus odio. Blanditiis et explicabo sed dignissimos non atque ut!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur esse
-        recusandae ducimus facilis doloribus repellat in libero quidem! Pariatur
-        natus corrupti delectus error necessitatibus consectetur at totam ab
-        modi quia! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Aspernatur animi corporis sapiente, porro illum error, consequuntur
-        itaque similique fugit, magni molestias cumque? Perspiciatis hic
-        consequuntur maxime nostrum. Possimus, est ad.
+      <div class="w-3/4 inline-block mt-2">
+        {{ t('dashboard.steps.step1.description') }}
       </div>
     </div>
 
@@ -30,10 +21,13 @@
           :description="t('dashboard.steps.step2.name')"
         />
       </div>
-      <div class="w-3/4 inline-block bg-green-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-        laborum sunt ut et earum debitis, harum sequi molestiae odit corporis
-        doloribus odio. Blanditiis et explicabo sed dignissimos non atque ut!
+      <div class="w-3/4 inline-block mt-2">
+        {{ t('dashboard.steps.step2.description') }}
+        <br />
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus, fuga
+        aspernatur incidunt accusantium adipisci ex at similique dolores.
+        Praesentium quae aut enim fugit quos quis harum saepe molestiae
+        exercitationem ex.
       </div>
     </div>
   </section>
@@ -71,29 +65,59 @@
 
   function getElementCoodinates(id: string) {
     const el = document.getElementById(id)
+    if (el === null) {
+      return { x: ref(0), y: ref(0) }
+    }
     const rect = el.getBoundingClientRect()
     return {
       x: ref(rect.x + rect.width / 2),
-      y: ref(rect.y),
+      y: ref(rect.y + rect.height / 2),
     }
   }
 
   function updateLineCoordinates() {
-    changeIndicator.value++
-    stepOneCoordinates = getElementCoodinates('step-one')
-    stepTwoCoordinates = getElementCoodinates('step-two')
+    /*
+    Hi reader of this code,
+
+    All JS developers have one thing in common, that is SUFERRING,
+    in which it leads to hacking with the code, and that why I'm writing this,
+    as you can see in the template the line takes its points coordinates from the given steps indicators elements,
+    WHICH might and will change on resizes and locale changing,
+    focus on the last one, because of the locale changing, the page has to reload,
+    and there's no such event to detect that, so, as far as the line knows,
+    the steps' elements never changed places (the huge shift from left to right),
+    and to make that happen, a timer is needed to ONLY update the line's points' coordinates
+    using the new and correct position, and 20ms is enough (i.e. not much noticable) to detect that (at least on my machine).
+
+    Hope that cleared the confusion for you!
+
+    PS: if the line's position doesn't update correctly, and that's what made you see this monstresity of a code,
+    kindly open a PR with the new value, and mention your devices specs.
+
+    Sincerely,
+    Baraa Al-Masri
+  */
+    setTimeout(() => {
+      stepOneCoordinates = getElementCoodinates('step-one')
+      stepTwoCoordinates = getElementCoodinates('step-two')
+      changeIndicator.value++
+    }, 20)
   }
 
-  onMounted(() => {
-    updateLineCoordinates()
-    window.addEventListener('resize', () => {
-      updateLineCoordinates()
-    })
-
+  function updateWindowDimensions() {
     windowDimensions = {
       width: window.innerWidth,
       height: window.innerHeight,
     }
+  }
+
+  onMounted(() => {
+    updateWindowDimensions()
+    updateLineCoordinates()
+    window.addEventListener('resize', () => {
+      updateLineCoordinates()
+      updateWindowDimensions()
+    })
   })
 </script>
 
