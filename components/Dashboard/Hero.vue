d55f31b6c6e7becb8bfd46c38269450a6c0875b3
Baraa Al-Masri <pub@mbaraa.com>
2023-10-08 14:31:41 +0300

    chore: updated description's font size

--- components/Dashboard/Hero.vue
+++ components/Dashboard/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-5 my-5 lg:my-20">
     <h1>{{ t('dashboard.title') }}</h1>
-    <p class="text-xl">{{ t('dashboard.description') }}</p>
+    <p class="text-4xl">{{ t('dashboard.description') }}</p>
   </div>
 </template>
 
