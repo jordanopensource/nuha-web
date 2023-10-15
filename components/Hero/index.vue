0a979280e5b82e8fc120efe5e51b84d44733c5ba
Mohamed Mansour <mohmans@pm.me>
2023-10-15 16:21:44 +0300

    fix(home-hero): text design

--- components/Hero/index.vue
+++ components/Hero/index.vue
@@ -1,23 +1,21 @@
 <template>
   <section class="h-full my-20">
     <div class="main">
-      <div class="w-full">
-        <div class="w-3/4">
-          <h1 class="text">
-            {{ $t('homeHero.title') }}
-          </h1>
-          <p class="text my-5 text-4xl">
-            {{ $t('homeHero.intro') }}
-          </p>
+      <div class="w-full p-4">
+        <h1 class="text">
+          {{ $t('homeHero.title') }}
+        </h1>
+        <p class="text my-5 text-4xl lg:w-2/3">
+          {{ $t('homeHero.intro') }}
+        </p>
 
-          <button
-            class="btn text-base"
-            @click="navigateTo(localePath('/dashboard'))"
-          >
-            {{ $t('homeHero.tryItOut') }}
-            <div class="arrow-icon"></div>
-          </button>
-        </div>
+        <button
+          class="btn text-base"
+          @click="navigateTo(localePath('/dashboard'))"
+        >
+          {{ $t('homeHero.tryItOut') }}
+          <div class="arrow-icon"></div>
+        </button>
       </div>
       <div class="w-full">
         <NuxtImg class="nuha-logo" src="/hero-logo.svg" />
@@ -34,13 +32,13 @@
 
 <style lang="postcss" scoped>
   .main {
-    @apply grid grid-cols-1 lg:grid-cols-2 place-items-center lg:gap-x-14;
+    @apply grid grid-cols-1 lg:grid-cols-2 place-items-center lg:gap-x-14 items-start;
   }
   .link {
     @apply grid grid-cols-1 place-items-center pt-7 italic text-xl font-medium underline text-nuha-fushia;
   }
   .text {
-    @apply text-nuha-grey leading-snug;
+    @apply text-nuha-grey leading-none;
   }
   .btn {
     @apply border my-3 p-3 flex items-center justify-center;
