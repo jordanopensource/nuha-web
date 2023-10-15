4674f89794a9f909b90fccf167097a8300a66df4
Mohamed Mansour <mohmans@pm.me>
2023-10-15 16:49:58 +0300

    refactor(home-hero): unused css

--- components/Hero/index.vue
+++ components/Hero/index.vue
@@ -34,9 +34,6 @@
   .main {
     @apply grid grid-cols-1 lg:grid-cols-2 place-items-center lg:gap-x-14 items-start;
   }
-  .link {
-    @apply grid grid-cols-1 place-items-center pt-7 italic text-xl font-medium underline text-nuha-fushia;
-  }
   .text {
     @apply text-nuha-grey leading-none;
   }
@@ -50,12 +47,5 @@
     @apply my-10 lg:my-0;
     @apply w-full;
     @apply w-full lg:w-[707px];
-  }
-  .btn:hover .arrow-icon {
-    @apply bg-[url('/icons/arrow-right-white.svg')];
-  }
-  .arrow-icon {
-    @apply mx-2 w-8 h-8 transform rtl:rotate-180 text-black;
-    @apply bg-[url('/icons/arrow-right.svg')] bg-cover;
   }
 </style>
