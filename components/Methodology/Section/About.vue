7a4204ef8a3f812d370a7d5e64090e312a0e80f6
Mohamed Mansour <mohmans@pm.me>
2023-10-19 13:49:26 +0300

    refactor(methodology-about): design fixes and code refactor

--- components/Methodology/Section/About.vue
+++ components/Methodology/Section/About.vue
@@ -1,43 +1,43 @@
 <template>
   <section>
-    <h2 class="font-medium text-[36px]">{{ $t('methodology.sections.about.firstParagraph.title') }}</h2>
-    <p class="font-medium text-[24px] leading-[1.2]">{{ $t('methodology.sections.about.firstParagraph.p1') }}</p>
-    <p class="font-medium text-[24px] leading-[1.2]">{{ $t('methodology.sections.about.firstParagraph.p2') }}</p>
+    <h2>{{ $t('methodology.sections.about.firstParagraph.title') }}</h2>
+    <p class="text">{{ $t('methodology.sections.about.firstParagraph.p1') }}</p>
+    <p class="text">{{ $t('methodology.sections.about.firstParagraph.p2') }}</p>
     <figure>
       <img
         src="/images/methodology-cycle.png"
         :alt="$t('methodology.sections.about.firstParagraph.fig1')"
       />
-      <figcaption class="mt-4">
+      <figcaption>
         <span class="font-semibold">{{ $t('methodology.figure') }} 1:</span>
         {{ $t('methodology.sections.about.firstParagraph.fig1') }}
       </figcaption>
     </figure>
   </section>
   <section>
-    <h2 class="font-medium text-[36px]">{{ $t('methodology.sections.about.secondParagraph.title') }}</h2>
-    <p class="font-medium text-[24px] leading-[1.2]">{{ $t('methodology.sections.about.secondParagraph.p1') }}</p>
-    <p class="font-medium text-[24px] leading-[1.2]">{{ $t('methodology.sections.about.secondParagraph.p2') }}</p>
+    <h2>{{ $t('methodology.sections.about.secondParagraph.title') }}</h2>
+    <p class="text">{{ $t('methodology.sections.about.secondParagraph.p1') }}</p>
+    <p class="text">{{ $t('methodology.sections.about.secondParagraph.p2') }}</p>
   </section>
-  <div class="footnotes py-8">
-    <p class="text-lg">{{ $t('methodology.footnotes') }}</p>
-    <ol class="list-decimal">
-      <li id="fn-1">
+  <div class="footnotes">
+    <p class="text-2xl">{{ $t('methodology.footnotes') }}</p>
+    <ol>
+      <li>
         <a href="http://" target="_blank" rel="noopener noreferrer">
           {{ $t('methodology.sections.about.footnotes.1') }}
         </a>
       </li>
-      <li id="fn-2">
+      <li>
         <a href="http://" target="_blank" rel="noopener noreferrer">
           {{ $t('methodology.sections.about.footnotes.2') }}
         </a>
       </li>
-      <li id="fn-2">
+      <li>
         <a href="http://" target="_blank" rel="noopener noreferrer">
           {{ $t('methodology.sections.about.footnotes.2') }}
         </a>
       </li>
-      <li id="fn-2">
+      <li>
         <a href="http://" target="_blank" rel="noopener noreferrer">
           {{ $t('methodology.sections.about.footnotes.1') }}
         </a>
@@ -51,19 +51,28 @@
 <style lang="postcss" scoped>
   div.container {
     section {
-      @apply flex flex-col gap-y-8 py-10;
+      @apply flex flex-col gap-y-10 pt-16 pb-6;
       @apply border-b border-nuha-grey-200;
-      h3 {
-        @apply font-semibold text-lg;
+      h2 {
+        @apply mb-4 font-normal text-4xl;
+      }
+      .text {
+        @apply font-normal text-2xl leading-[1.2];
+      }
+      figure {
+        figcaption {
+          @apply mt-6 font-IBMPlexSansArabic text-sm;
+        }
       }
     }
     .footnotes {
+      @apply mb-6 mt-9;
       ol {
         counter-reset: custom;
         list-style-type: none;
 
         li {
-          @apply my-4;
+          @apply my-4 font-IBMPlexSansArabic text-sm font-normal;
           &::before {
             content: '[' counter(custom) '] ';
             counter-increment: custom;
