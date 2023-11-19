7cafe21fad88aed034560024f8e93b497ed8ee8e
Baraa Al-Masri <pub@mbaraa.com>
2023-11-19 15:01:29 +0300

    fix: removed post checking in xls(x) files

--- server/utils/file-parser.ts
+++ server/utils/file-parser.ts
@@ -1,4 +1,4 @@
-import {parse} from 'csv-parse/sync'
+import { parse } from 'csv-parse/sync'
 import xlsx from 'node-xlsx'
 
 export interface FileParser {
@@ -13,58 +13,60 @@ export enum FileType {
 
 export function getFileType(fileTypeText: string): FileType {
   switch (fileTypeText.toLowerCase()) {
-  case 'xlsx':
-    return FileType.XLSX;
-  case 'csv':
-    return FileType.CSV;
-  default:
-    return FileType.INVALID
+    case 'xlsx':
+    case 'xls':
+      return FileType.XLSX
+    case 'csv':
+      return FileType.CSV
+    default:
+      return FileType.INVALID
   }
 }
 
 export async function getFileParser(fileType: FileType): Promise<FileParser> {
   switch (fileType) {
-  case FileType.CSV:
-    return resolve(CsvFileParser.instance);
-  case FileType.XLSX:
-    return resolve(XlsxFileParser.instance);
-  default:
-    return reject('apiResponse.invalidFileType')
+    case FileType.CSV:
+      return resolve(CsvFileParser.instance)
+    case FileType.XLSX:
+      return resolve(XlsxFileParser.instance)
+    default:
+      return reject('apiResponse.invalidFileType')
   }
 }
 
 class CsvFileParser implements FileParser {
   private static _instance = new CsvFileParser()
 
-      private constructor() {}
+  private constructor() {}
 
-  static get instance(): CsvFileParser{return CsvFileParser._instance}
+  static get instance(): CsvFileParser {
+    return CsvFileParser._instance
+  }
 
   async parseFile(file: Buffer): Promise<SingleComment[]> {
     const data = new Array<SingleComment>()
     const records = parse(file, {
-      delimiter : ',',
-      columns : true,
-      skip_empty_lines : true,
-      trim : true,
+      delimiter: ',',
+      columns: true,
+      skip_empty_lines: true,
+      trim: true,
     })
 
     if (!records) {
       return reject('apiResponse.invalidInputData')
     }
 
-    let recordKeys: string[];
+    let recordKeys: string[]
     for (const record of records) {
       recordKeys = Object.keys(record)
       if (!recordKeys.includes('comment')) {
         return reject('apiResponse.missingComment')
       }
-      if (!recordKeys.includes('post')) {
-        return reject('apiResponse.missingPost')
+      if (!record.comment || record.comment.length === 0) {
+        continue
       }
       data.push({
-        comment : record.comment,
-        post : record.post,
+        comment: record.comment,
       })
     }
 
@@ -75,34 +77,38 @@ class CsvFileParser implements FileParser {
 class XlsxFileParser implements FileParser {
   private static _instance = new XlsxFileParser()
 
-      private constructor() {}
+  private constructor() {}
 
-  static get instance(): XlsxFileParser{return XlsxFileParser._instance}
+  static get instance(): XlsxFileParser {
+    return XlsxFileParser._instance
+  }
 
   // WARNING: what you're about to read will melt you brain down,
   // guess xlsx is just bad everywhere!
   async parseFile(file: Buffer): Promise<SingleComment[]> {
     const records = xlsx.parse(file)
-    if (!records || !records[0] || !records[0].data ||
-        records[0].data.length === 0) {
+    if (
+      !records ||
+      !records[0] ||
+      !records[0].data ||
+      records[0].data.length === 0
+    ) {
       return reject('apiResponse.invalidInputData')
     }
 
     if (!records[0].data[0].includes('comment')) {
       return reject('apiResponse.missingComment')
     }
-    if (!records[0].data[0].includes('post')) {
-      return reject('apiResponse.missingPost')
-    }
 
     const commentIndex = records[0].data[0].indexOf('comment')
-    const postIndex = records[0].data[0].indexOf('post')
 
     const data = new Array<SingleComment>()
-    for (const record of records[0].data.slice(1)) {
+    for (const record of records[0].data) {
+      if (!record || record.length === 0) {
+        continue
+      }
       data.push({
-        comment : record[commentIndex],
-        post : record[postIndex],
+        comment: record[commentIndex],
       })
     }
 
