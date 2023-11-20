bd6344e9260bebee2c4e98b41c5fd176a274c8af
Baraa Al-Masri <pub@mbaraa.com>
2023-11-21 00:27:53 +0300

    merge: fixed conflicts 8

--- lang/en.ts
+++ lang/en.ts
@@ -89,11 +89,6 @@ export default {
       },
     },
   },
-  findings: {
-    title: 'Findings',
-    // TODO: update description
-    description: 'Our text models are advanced language processing tools that can generate, classify, and summarize text with high levels of coherence and accuracy.'
-  },
 
   statistical: {
     info: "The General Aspect focuses on effective advocacy and campaigning towards online hate speech, especially those targeting women public spaces in Jordan including women activists, human rights defenders and journalists, social media influencers and women in, on quantitative data. Hence, JOSA Will be identifying at least 100 Women' activist in Jordan based on a specific selection criterion to  their social media accounts (Facebook and twitter) and hate-speech attacks they receive. JOSA researchers Will collect this data using different methods, classify it into different categories while taking into consideration the behaviors identified.",
