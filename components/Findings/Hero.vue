0accbdc843405ca4ff31405aaa3f0e08da019f32
Baraa Al-Masri <pub@mbaraa.com>
2023-11-21 13:10:58 +0300

    chore: added findings hero section

---
+++ components/Findings/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="findings-border flex flex-col gap-y-14 items-center justify-center"
+  >
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
+      <h1>{{ t('findings.title') }}</h1>
+      <p class="text-4xl">
+        {{ t('findings.description') }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const { t } = useI18n()
+</script>
+
+<style lang="postcss" scoped></style>
