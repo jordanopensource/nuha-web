1e8ef7a8c010f0def2b3168cd1ca09fe2b95f5bc
thamudi <tamim_hamoudi@outlook.com>
2024-11-27 12:28:42 +0300

    build: packages cleanup and update

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,11 +1,10 @@
 import { resolve, reject } from './promise-handlers'
-import { FileParser, getFileParser, FileType, getFileType } from './file-parser'
+import { getFileParser, FileType, getFileType } from './file-parser'
 import { predictCommentsResults } from './nuha-api-request'
 
 export {
   resolve,
   reject,
-  FileParser,
   getFileParser,
   FileType,
   getFileType,
