445669634b9ce16a1e7b53146e41f7f08605e49d
thamudi <tamim_hamoudi@outlook.com>
2024-11-28 12:18:53 +0300

    patch: fix regex potential exponential backtracking

--- server/api/upload-comment.post.ts
+++ server/api/upload-comment.post.ts
@@ -34,7 +34,7 @@ async function verifyData(input: SingleComment): Promise<void> {
   }
 
   const emailPattern = new RegExp(
-    /(?:[a-zA-Z\d!#$%&'*+/=?^_{|}~-]+(?:\.[a-zA-Z\d!#$%&'*+/=?^_{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-zA-Z\d](?:[a-zA-Z\d-]*[a-zA-Z\d])?\.)+[a-zA-Z\d](?:[a-zA-Z\d-]*[a-zA-Z\d])?|\[(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?|[a-zA-Z\d-]*[a-zA-Z\d]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])/gm
+    /^(?:[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/gm
   )
   if (input.comment.match(emailPattern)) {
     return reject('apiResponse.commentContainsEmail')
