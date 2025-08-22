bdf23d3d104c2be8ea4270108b02841c1802b680
Mohamed Mansour <mohmans@pm.me>
2025-08-22 21:23:30 +0300

    feat: add publications page — WIP

---
+++ pages/[[region]]/publications/[slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    This is a single publication page
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/[[region]]/publications/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    This is all publications page
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
