380eacecd317d43b61a6c09c8d1364e49e33bb62
Mohamed Mansour <mohmans@pm.me>
2025-08-23 00:09:19 +0300

    fix: translate analyze page title and subtitle

--- pages/[[region]]/analyze/index.vue
+++ pages/[[region]]/analyze/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="page-container">
     <UiPageHeading 
-      title="Analyze Comments"
+      :title="$t('analyze.page.title')"
       use-h1
-      subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris." 
+      :subtitle="$t('analyze.page.subtitle')" 
     />
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4 items-start my-5">
       <analyze-form />
