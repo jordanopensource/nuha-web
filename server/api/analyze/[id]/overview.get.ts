c3364e1d163e451c8dede5a5b26c629ea2bfab76
Mohamed Mansour <mohmans@pm.me>
2026-02-21 15:23:14 +0300

    feat: add endpoint to get analysis overview

---
+++ server/api/analyze/[id]/overview.get.ts
@@ -0,0 +1,26 @@
+import { AnalysisQueue } from '~/server/utils/analysis-queue'
+
+export default defineEventHandler(async (event) => {
+  const id = getRouterParam(event, 'id')
+
+  if (!id) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Analysis ID is required',
+    })
+  }
+
+  const overview = await AnalysisQueue.getOverview(id)
+
+  if (!overview) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Analysis job not found',
+    })
+  }
+
+  return {
+    success: true,
+    data: overview
+  }
+})
