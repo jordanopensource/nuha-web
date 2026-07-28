2d596c37096e40483d8ae121b0e537d37c2684c3
Mo Mansour <mohmans@pm.me>
2026-07-28 15:33:37 +0300

    lint: fix eslint issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,13 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-export default withNuxt([eslintPluginPrettierRecommended])
+export default withNuxt([
+  eslintPluginPrettierRecommended,
+  {
+    name: 'nuha/primevue-pt-hyphenation',
+    rules: {
+      // PrimeVue passthrough (`pt:`) attributes must stay camelCase to match
+      'vue/attribute-hyphenation': ['warn', 'always', { ignore: ['pt:'] }],
+    },
+  },
+])
